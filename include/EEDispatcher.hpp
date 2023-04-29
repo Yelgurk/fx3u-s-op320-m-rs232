6bec0304939c1b65e947260f04f8b4b41ce8ee57
Yelgurk <maksim.krugley@bk.ru>
2023-04-29 12:01:35 +0300

    resume on laptop

--- include/EEDispatcher.hpp
+++ include/EEDispatcher.hpp
@@ -18,7 +18,7 @@ public:
     EEUnit ee_blowgun_preset_1 = EEUnit(0x0000, &ee24c64, false),
            ee_blowgun_preset_2 = EEUnit(0x0001, &ee24c64, false),
            ee_blowgun_preset_3 = EEUnit(0x0002, &ee24c64, false),
-           ee_blowgun_preset_4 = EEUnit(0x0003, &ee24c64, false),
+           ee_blowgun_preset_4 = EEUnit(0x0003, &ee24c64, false);
            
             
     bool init();
